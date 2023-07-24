ab92af0688082da2570868f3dff78c4d23ad22a6
mateoQuotteri <102633057+mateoQuotteri@users.noreply.github.com>
2023-07-24 19:00:43 -0300

    Add files via upload

---
+++ javaPractices/src/Main.java
@@ -0,0 +1,34 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    /*
+    public static void main(String[] args) {
+        // Press Alt+Intro with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+    }
+    Ejercicio 1
+Definir dos cadenas de texto. Comprobar si son iguales o distintas e indicar mediante un
+mensaje.
+
+Ejercicio 2
+
+Definir dos Integer y determinar cuál es mayor, cuál es menor o si son iguales.
+Informar el resultado obtenido.*/
+
+    public static void main(String[] args) {
+        String name1 = "Jorge";
+        String name2 = "Jorge";
+
+        if (name1.equals(name2)) {
+            System.out.println("Los nombres son iguales");
+        } else {
+            System.out.println("Los nombres NO son iguales");
+        }
+
+    }
+}
+
+
+
