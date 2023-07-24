d2868158676eba8b2f959ec8e44de37d375d6968
Mateo Quotteri <quotterimateo@gmail.com>
2023-07-24 20:47:04 -0300

    connection with github and first class of java

--- javaPractices/src/Main.java
+++ javaPractices/src/Main.java
@@ -15,7 +15,7 @@ mensaje.
 Ejercicio 2
 
 Definir dos Integer y determinar cuál es mayor, cuál es menor o si son iguales.
-Informar el resultado obtenido.*/
+Informar el resultado obtenido.
 
     public static void main(String[] args) {
         String name1 = "Jorge";
@@ -26,6 +26,28 @@ Informar el resultado obtenido.*/
         } else {
             System.out.println("Los nombres NO son iguales");
         }
+
+    }*/
+
+    /*
+          Para representar cada una de las características de las mascotas, crear una variable y darle
+  el valor correspondiente en el IDE IntelliJ. No olvidar darle el tipo adecuado a lo que
+  queremos que contenga. Por cada mascota de la veterinaria que figura en la siguiente
+  tabla, vamos a tener que mostrar la información del animal. La información va a mostrarse
+  como el siguiente ejemplo:
+  “Manchitas tiene 2 años”
+  “Manchitas come un kilo y medio y hace guau guau”
+          * */
+    public static void main(String[] args) {
+
+
+        String nombre = "Manchitas";
+        int edad =2;
+        double comida = 1.5;
+        String ladrido = "Guau Guau Guau";
+        System.out.println(nombre + " tiene " + edad + " años");
+        System.out.println(nombre + " come " + comida+ " kg y hace " + ladrido);
+
 
     }
 }
---
+++ javaPractices/src/firstClass.java
@@ -0,0 +1,22 @@
+public class firstClass {
+    public static void firstClass(String[] args) {
+        /*
+        Para representar cada una de las características de las mascotas, crear una variable y darle
+el valor correspondiente en el IDE IntelliJ. No olvidar darle el tipo adecuado a lo que
+queremos que contenga. Por cada mascota de la veterinaria que figura en la siguiente
+tabla, vamos a tener que mostrar la información del animal. La información va a mostrarse
+como el siguiente ejemplo:
+“Manchitas tiene 2 años”
+“Manchitas come un kilo y medio y hace guau guau”
+        * */
+
+        String nombre = "Manchitas";
+        int edad =2;
+        double comida = 1.5;
+        String ladrido = "Guau Guau Guau";
+        System.out.println(nombre + " tiene " + edad + " años");
+        System.out.println(nombre + " come " + comida+ " kg y hace " + ladrido);
+
+
+    }
+}
